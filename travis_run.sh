b121f6f4ccd17bbe6380ff77c1df501588498199
Andreas Heger <andreas.heger@gmail.com>
2016-02-17 21:12:51 +0000

    test travis setup

---
+++ travis_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+if [[ $TRAVIS_OS_NAME = "linux" ]]
+then
+    ./run_tests_travis.sh
+else
+    export PATH=/anaconda/bin:$PATH
+    # build packages
+    scripts/build-packages.py --repository . --packages `cat osx-whitelist.txt`
+fi
---
+++ travis_setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+if [[ $TRAVIS_OS_NAME = "linux" ]]
+then
+    docker pull bioconda/bioconda-builder
+else
+    # install Rust
+    curl -sSf http://static.rust-lang.org/rustup.sh | sudo sh -s -- --channel=nightly -y --disable-sudo
+
+    # install conda
+    curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+    sudo bash Miniconda3-latest-MacOSX-x86_64.sh -b -p /anaconda
+    sudo chown -R $USER /anaconda
+    mkdir -p /anaconda/conda-bld/osx-64 # workaround for bug in current conda
+    mkdir -p /anaconda/conda-bld/linux-64 # workaround for bug in current conda
+    export PATH=/anaconda/bin:$PATH
+    conda install -y conda conda-build anaconda-client pyyaml toolz jinja2 nose
+    conda index /anaconda/conda-bld/linux-64 /anaconda/conda-bld/osx-64
+
+    # setup bioconda channel
+    conda config --add channels bioconda
+    conda config --add channels r
+    conda config --add channels file://anaconda/conda-bld
+
+    conda install -y toposort
+fi
